77409a676e06e3690b3d07d33dba6ef139caeadc
Nguyen Trung Hung <hhuhnhg@gmail.com>
2024-10-10 15:57:41 +0700

    Update Message 1.1

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -265,13 +265,15 @@ class UsersService {
         password: 0,
         email_verify_token: 0,
         forgot_password_token: 0,
+        created_at: 0,
+        updated_at: 0,
         twitter_circle: 0,
         bio: 0,
         location: 0,
         website: 0,
         avatar: 0,
         cover_photo: 0,
-        date_of_birth: 0,
+        date_of_birth: 0
       })
       .toArray()
   }
